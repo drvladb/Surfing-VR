cae7919a6790ded4523141cafd5e0c663d582189
apranvoku <pranvoku.ardit@gmail.com>
2019-06-01 10:56:39 -0400

    Added score functionality

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    private Text Score;
+    private int ScoreInt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = this.GetComponent<Text>();
+        ScoreInt = 0;
+        Score.text = ScoreInt.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            ScoreInt++;
+            Score.text = ScoreInt.ToString();
+        }
+    }
+}
