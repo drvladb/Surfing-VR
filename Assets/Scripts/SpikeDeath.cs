c2ae548a998a18b2bea8419ac020d1a1d33da788
vlad <drvladb@gmail.com>
2019-06-05 17:02:50 -0400

    DeathThing

--- Assets/Scripts/SpikeDeath.cs
+++ Assets/Scripts/SpikeDeath.cs
@@ -1,22 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class ToLevels : MonoBehaviour
+public class SpikeDeath : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "Player")
-        {
-            SceneManager.LoadScene(3);
-        }
-    }
+
     // Update is called once per frame
     void Update()
     {
