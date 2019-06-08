948122e5ef314fc54d0bb89a696e32d658b4d433
apranvoku <pranvoku.ardit@gmail.com>
2019-06-08 10:09:56 -0400

    Revert Changes

---
+++ Assets/Scripts/BuyObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;//Hot Garbadge (basicly s**t)
+using UnityEngine.UI;
+
+public class BuyObject : MonoBehaviour
+{
+    Button myButton;
+    private int cost = 25;
+    public GameObject sold;
+    public bool objectsold;
+    public int score;
+    public GameObject scoreobject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = PlayerPrefs.GetInt("Score", 0);
+    }
+    private void Awake()
+    {
+        myButton = GetComponent<Button>(); // <-- you get access to the button component here
+
+        myButton.onClick.AddListener(() => { myFunctionForOnClickEvent(); });  // <-- you assign a method to the button OnClick event here
+        //myButton.onClick.AddListener(() => { myAnotherFunctionForOnClickEvent("stringValue", 3); }); // <-- you can assign multiple methods
+    }
+    // Update is called once per frame
+    public void myFunctionForOnClickEvent()
+    {
+        this.gameObject.SetActive(false);
+        sold.SetActive(true);
+        PlayerPrefs.SetInt("GotBall1", 1);
+        PlayerPrefs.SetInt("Score", score - 25);
+        score = score - 25;
+        scoreobject.GetComponent<Text>().text = score.ToString();
+    }
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("GotBall1", 0) == 1)
+        {
+            this.gameObject.SetActive(false);
+            sold.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/ReturnToPlaygroundButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReturnToPlaygroundButton : MonoBehaviour
+{
+    Button myButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myButton = GetComponent<Button>(); // <-- you get access to the button component here
+
+        myButton.onClick.AddListener(() => { myFunctionForOnClickEvent(); });  // <-- you assign a method to the button OnClick event here
+        //myButton.onClick.AddListener(() => { myAnotherFunctionForOnClickEvent("stringValue", 3); }); // <-- you can assign multiple methods
+    }
+    public void myFunctionForOnClickEvent()
+    {
+        SceneManager.LoadScene(1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
